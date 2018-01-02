a1db81b9215ef811a182eb59fe37e82aa054de01
Dmitriy Beglov <ditransler@gmail.com>
2018-01-02 18:32:55 +0400

    Finished lesson #11 - Fetching Data From an API & async await

--- src/App.js
+++ src/App.js
@@ -4,34 +4,32 @@ import './App.css';
 
 import Movie from './Movie';
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars',
-    desc: 'A space movie'
-  },
-  {
-    id: 2,
-    title: 'Spider Man'
-  },
-  {
-    id: 3,
-    title: '36th Chamber of Shaolin'
-  },
-  {
-    id: 4,
-    title: '5 Deadly Venoms'
-  }
-];
-
 class App extends Component {
+
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_THEMOVIEDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`);
+      const movies = await res.json();
+
+      this.setState({
+        movies: movies.results
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -5,21 +5,14 @@ export default class Movie extends Component {
     static propTypes = {
         movie: PropTypes.shape({
             title: PropTypes.string.isRequired
-        }),
-        desc: PropTypes.string
-    }
-
-    static defaultProps = {
-        desc: 'A description is not available'
+        })
     }
 
     render() {
         const {title} = this.props.movie;
-        const {desc} = this.props;
         return (
             <div>
                 <h3>{title}</h3>
-                <p>{desc}</p>
             </div>
         );
     }
